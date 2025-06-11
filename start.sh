0e74e18cc05a2be959af6fa2c7889f79fe3089d0
Mhalid256 <musasizihalid@gmail.com>
2025-06-11 08:23:46 +0300

    Make start.sh executable for Railway

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+gunicorn djecommerce.wsgi:application --bind 0.0.0.0:8080
